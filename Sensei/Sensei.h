cea087eb29d5135ef1420413100425a3f873b021
Jacobo Koenig <jacobo360@gmail.com>
2019-03-10 10:47:44 +0000

    First Commit

---
+++ Sensei/Sensei.h
@@ -0,0 +1,19 @@
+//
+//  Sensei.h
+//  Sensei
+//
+//  Created by Jacobo Koenig on 3/10/19.
+//  Copyright © 2019 Binary Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sensei.
+FOUNDATION_EXPORT double SenseiVersionNumber;
+
+//! Project version string for Sensei.
+FOUNDATION_EXPORT const unsigned char SenseiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sensei/PublicHeader.h>
+
+
